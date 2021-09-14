36ce187234672143965240ad464c1d80ff64fb17
calanchoe <lizakrivosheyyes@gmail.com>
2021-09-14 23:06:15 +0300

    add new table

---
+++ database/migrations/3033_08_31_204503_create_views_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp('viewed_at')->default(new Expression('NOW()'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+}
